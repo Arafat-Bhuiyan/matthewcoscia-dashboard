adc04fafccb154acd245705db8342bc381836538
Arafat-Bhuiyan <arafatbhuiyan41@gmail.com>
2026-03-04 15:32:17 +0600

    base url changed

--- src/redux/api/api.js
+++ src/redux/api/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://10.10.13.20:8005/api/v1",
+  baseUrl: "https://api.coachcal.com.au/api/v1",
   prepareHeaders: (headers, { getState }) => {
     // Try to get token from Redux state
     const token = getState().auth?.access || localStorage.getItem("access");
